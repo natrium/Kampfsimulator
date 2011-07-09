c1f395dc267e77755eb3499b72d5471f7d949e10
Fabian Müller <fmueller83@googlemail.com>
2011-07-09 19:17:36 +0200

    Multikill

--- main.rb
+++ main.rb
@@ -3,12 +3,18 @@ require_relative 'lib/fight'
 require_relative 'lib/hero'
 require_relative 'lib/monster'
 
-conan = Hero.new :name => "Conan the barbarian"
+conan = Hero.new :name => "Conan the barbarian", :str => 4, :con => 2
 stalker = Monster.new :name => "Stalker"
 snakelady = Monster.new :name => "Snake-Lady"
+bodycount = 0
 
+begin
+    monstaRRR = stalker.clone
+    Fight::fight conan, monstaRRR
+    bodycount += 1 if monstaRRR.is_dead?
+end while conan.is_alive?
 
-Fight::fight conan, stalker
+print conan.name + " has slain #{bodycount} " + stalker.name
+print "s" if bodycount >  1
+puts
 
-puts conan
-puts stalker
